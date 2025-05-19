5edecac50185f28ce3631e790c5fc4bd6d93d8d5
RETXSD <andrianandrian761@gmail.com>
2025-05-19 19:11:25 +0700

    Pesan commit pertama

---
+++ UTS/src/database/seeders/LogoSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Logo;
+
+class LogoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        logo::create([
+            'image' => 'https://example.com/image.jpg',
+        ]);
+    }
+}
---
+++ UTS/src/database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'name' => 'Sample Product',
+            'description' => 'This is a sample product description.',
+            'image' => '',
+            'price' => 'RP 100.000', // Added price
+            'stock' => 100, 
+            'category' => 'Sample Category', // Added category
+        ]);
+    }
+}
